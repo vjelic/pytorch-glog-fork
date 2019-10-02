6c9807a2762a21627bcf8cea78b46b567d0fe99a
ashishfarmer <ashish.farmer@amd.com>
2019-10-02 18:35:22 +0000

    remove cudart depencency for HIP sources

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -491,7 +491,8 @@ def CUDAExtension(name, sources, *args, **kwargs):
     kwargs['library_dirs'] = library_dirs
 
     libraries = kwargs.get('libraries', [])
-    libraries.append('cudart')
+    if not HIP_COMP:
+        libraries.append('cudart')
     if IS_WINDOWS:
         libraries.append('c10')
         libraries.append('c10_cuda')
