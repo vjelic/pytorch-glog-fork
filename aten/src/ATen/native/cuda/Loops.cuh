038b94dd2c8b0229bd008a08d7faa7715e02c567
Jeff Daily <jeff.daily@amd.com>
2020-07-29 18:06:11 +0000

    unify ROCmLoops and CUDALoops

--- aten/src/ATen/native/cuda/Loops.cuh
+++ aten/src/ATen/native/cuda/Loops.cuh
@@ -39,17 +39,7 @@ static OffsetCalculator<1> make_output_offset_calculator(const TensorIterator& i
 
 }}  // namespace at::native
 
-// Note:
-// CUDA and ROCm get diverged in this PR:
-//   https://github.com/pytorch/pytorch/pull/32383
-// Because for some reason trying to enable vectorized
-// memory access introduce regression on ROCm.
-
-#ifndef __HIP_PLATFORM_HCC__
 #include <ATen/native/cuda/CUDALoops.cuh>
-#else
-#include <ATen/native/cuda/ROCmLoops.cuh>
-#endif
 
 namespace at { namespace native {
 
