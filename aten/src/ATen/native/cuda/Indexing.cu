pytorch/pytorch

fused index_add + scale (#699)

aten/src/ATen/native/cuda/Indexing.cu
@@ -311,7 +311,8 @@ __global__ void indexAddSmallIndex(cuda::detail::TensorInfo<T, IndexType> dst,
                                    int dstAddDim,
                                    int srcAddDim,
                                    IndexType innerSize,
-                                   int64_t dstAddDimSize) {
+                                   int64_t dstAddDimSize,
+                                   T alpha) {
   // In order to avoid reloading the index that we are copying, load
   // it once to handle all of the points that are being selected, so
   // it can be reused as much as possible. This kernel is chosen when
@@ -336,7 +337,7 @@ __global__ void indexAddSmallIndex(cuda::detail::TensorInfo<T, IndexType> dst,
           cuda::detail::IndexToOffset<T, IndexType, SrcDim>::get(linearIndex, src);
       srcOffset += srcIndex * src.strides[srcAddDim];
 
-      gpuAtomicAdd(&dst.data[dstOffset], src.data[srcOffset]);
+      gpuAtomicAdd(&dst.data[dstOffset], alpha * src.data[srcOffset]);
     }
   }
 }
@@ -356,7 +357,8 @@ __global__ void indexAddLargeIndex(cuda::detail::TensorInfo<T, IndexType> dst,
                                    int srcAddDim,
                                    IndexType totalSize,
                                    IndexType innerSize,
-                                   int64_t dstAddDimSize) {
+                                   int64_t dstAddDimSize,
+                                   T alpha) {
   // We stride over the output including the indexed dimension
   // (totalSize), and calculate the destination index point based on that
   for (IndexType linearIndex = blockIdx.x * blockDim.x + threadIdx.x;
@@ -385,7 +387,7 @@ __global__ void indexAddLargeIndex(cuda::detail::TensorInfo<T, IndexType> dst,
       cuda::detail::IndexToOffset<T, IndexType, SrcDim>::get(elementInSlice, src);
     srcOffset += srcIndex * src.strides[srcAddDim];
 
-    gpuAtomicAdd(&dst.data[dstOffset], src.data[srcOffset]);
+    gpuAtomicAdd(&dst.data[dstOffset], alpha * src.data[srcOffset]);
   }
 }
 
@@ -424,7 +426,7 @@ bool indexShouldBeMajor(cuda::detail::TensorInfo<scalar_t, unsigned int> &info,
   return false;
 }
 
-Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const Tensor & source) {
+Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const Tensor & source, const at::Scalar alpha) {
   // Nondeterministic because of atomicAdd usage
   globalContext().alertNotDeterministic("index_add_cuda_");
   dim = maybe_wrap_dim(dim, self.dim());
@@ -471,7 +473,7 @@ Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const 
   indexAddSmallIndex<TENSOR_TYPE, TYPE, SELF_DIM, SOURCE_DIM, IDX_DIM> \
     <<<smallIndexGrid, smallIndexBlock, 0, stream>>>(   \
       selfInfo, sourceInfo, indexInfo,                    \
-      selfAddDim, sourceAddDim, sliceSize, selfAddDimSize);
+      selfAddDim, sourceAddDim, sliceSize, selfAddDimSize, alpha_scalar);
 
 #define LARGE_INDEX(TENSOR_TYPE, TYPE,                        \
                     SELF_DIM, SOURCE_DIM, IDX_DIM, IDX_IS_MAJOR)  \
@@ -481,7 +483,7 @@ Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const 
       selfInfo, sourceInfo, indexInfo,                          \
       selfAddDim, sourceAddDim, sourceTotalSize,                     \
       (IDX_IS_MAJOR) ? sliceSize : numIndex,                \
-      selfAddDimSize);
+      selfAddDimSize, alpha_scalar);
 
   dim3 smallIndexGrid(std::min(THCCeilDiv(sliceSize, (ptrdiff_t)128), (ptrdiff_t)(mpc * 8)));
   dim3 smallIndexBlock(std::min(sliceSize, (ptrdiff_t)128));
@@ -507,6 +509,8 @@ Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const 
         auto indexInfo =
          cuda::detail::getTensorInfo<int64_t, unsigned int>(index);
         indexInfo.collapseDims();
+
+        auto alpha_scalar = alpha.to<scalar_t>();
 
         // A reasonable choice for when to have each thread iterate over
         // index to choose
@@ -559,6 +563,8 @@ Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const 
         cuda::detail::TensorInfo<int64_t, uint64_t> indexInfo =
           cuda::detail::getTensorInfo<int64_t, uint64_t>(index);
         indexInfo.collapseDims();
+
+        auto alpha_scalar = alpha.to<scalar_t>();
 
         LARGE_INDEX(scalar_t, uint64_t, -1, -1, -1, true);
       });

vjelic/pytorch-glog-fork

aten/src/ATen/native/cuda/Indexing.cu
@@ -310,7 +310,8 @@ __global__ void indexAddSmallIndex(cuda::detail::TensorInfo<T, IndexType> dst,
                                    int dstAddDim,
                                    int srcAddDim,
                                    IndexType innerSize,
-                                   int64_t dstAddDimSize) {
+                                   int64_t dstAddDimSize,
+                                   T alpha) {
   // In order to avoid reloading the index that we are copying, load
   // it once to handle all of the points that are being selected, so
   // it can be reused as much as possible. This kernel is chosen when
@@ -335,7 +336,7 @@ __global__ void indexAddSmallIndex(cuda::detail::TensorInfo<T, IndexType> dst,
           cuda::detail::IndexToOffset<T, IndexType, SrcDim>::get(linearIndex, src);
       srcOffset += srcIndex * src.strides[srcAddDim];
 
-      gpuAtomicAddNoReturn(&dst.data[dstOffset], src.data[srcOffset]);
+      gpuAtomicAddNoreturn(&dst.data[dstOffset], alpha * src.data[srcOffset]);
     }
   }
 }
@@ -355,7 +356,8 @@ __global__ void indexAddLargeIndex(cuda::detail::TensorInfo<T, IndexType> dst,
                                    int srcAddDim,
                                    IndexType totalSize,
                                    IndexType innerSize,
-                                   int64_t dstAddDimSize) {
+                                   int64_t dstAddDimSize,
+                                   T alpha) {
   // We stride over the output including the indexed dimension
   // (totalSize), and calculate the destination index point based on that
   for (IndexType linearIndex = blockIdx.x * blockDim.x + threadIdx.x;
@@ -384,7 +386,7 @@ __global__ void indexAddLargeIndex(cuda::detail::TensorInfo<T, IndexType> dst,
       cuda::detail::IndexToOffset<T, IndexType, SrcDim>::get(elementInSlice, src);
     srcOffset += srcIndex * src.strides[srcAddDim];
 
-    gpuAtomicAddNoReturn(&dst.data[dstOffset], src.data[srcOffset]);
+    gpuAtomicAddNoReturn(&dst.data[dstOffset], alpha * src.data[srcOffset]);
   }
 }
 
@@ -423,7 +425,9 @@ bool indexShouldBeMajor(cuda::detail::TensorInfo<scalar_t, unsigned int> &info,
   return false;
 }
 
-Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const Tensor & source) {
+Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const Tensor & source, const at::Scalar alpha) {
+  // Nondeterministic because of atomicAdd usage
+  globalContext().alertNotDeterministic("index_add_cuda_");
   dim = maybe_wrap_dim(dim, self.dim());
 
   TensorArg self_arg{self, "self", 1}, index_arg{index, "index", 3}, source_arg{source, "source", 4};
@@ -468,7 +472,7 @@ Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const 
   indexAddSmallIndex<TENSOR_TYPE, TYPE, SELF_DIM, SOURCE_DIM, IDX_DIM> \
     <<<smallIndexGrid, smallIndexBlock, 0, stream>>>(   \
       selfInfo, sourceInfo, indexInfo,                    \
-      selfAddDim, sourceAddDim, sliceSize, selfAddDimSize);
+      selfAddDim, sourceAddDim, sliceSize, selfAddDimSize, alpha_scalar);
 
 #define LARGE_INDEX(TENSOR_TYPE, TYPE,                        \
                     SELF_DIM, SOURCE_DIM, IDX_DIM, IDX_IS_MAJOR)  \
@@ -478,7 +482,7 @@ Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const 
       selfInfo, sourceInfo, indexInfo,                          \
       selfAddDim, sourceAddDim, sourceTotalSize,                     \
       (IDX_IS_MAJOR) ? sliceSize : numIndex,                \
-      selfAddDimSize);
+      selfAddDimSize, alpha_scalar);
 
   dim3 smallIndexGrid(std::min(THCCeilDiv(sliceSize, (ptrdiff_t)128), (ptrdiff_t)(mpc * 8)));
   dim3 smallIndexBlock(std::min(sliceSize, (ptrdiff_t)128));
@@ -504,6 +508,8 @@ Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const 
         auto indexInfo =
          cuda::detail::getTensorInfo<int64_t, unsigned int>(index);
         indexInfo.collapseDims();
+
+        auto alpha_scalar = alpha.to<scalar_t>();
 
         // A reasonable choice for when to have each thread iterate over
         // index to choose
@@ -556,6 +562,8 @@ Tensor& index_add_cuda_(Tensor & self, int64_t dim, const Tensor & index, const 
         cuda::detail::TensorInfo<int64_t, uint64_t> indexInfo =
           cuda::detail::getTensorInfo<int64_t, uint64_t>(index);
         indexInfo.collapseDims();
+
+        auto alpha_scalar = alpha.to<scalar_t>();
 
         LARGE_INDEX(scalar_t, uint64_t, -1, -1, -1, true);
       });