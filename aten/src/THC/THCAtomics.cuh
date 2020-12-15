fbf7445e0d3378d3362078c26a05f43712967f67
sunway513 <peng.sun@amd.com>
2020-12-15 12:16:48 -0600

    update the atomicAddNoRet api

--- aten/src/THC/THCAtomics.cuh
+++ aten/src/THC/THCAtomics.cuh
@@ -310,7 +310,7 @@ static inline __device__ void atomicAddNoReturn(double *address, double val) { g
 #ifdef __gfx908__
 static inline __device__ bool __hip_is_shared(const __attribute__((address_space(0))) void*) asm("llvm.amdgcn.is.shared");
 /* return tyre of intrinsic was void in earlier ROCm versions */
-static inline __device__ void atomicAddNoRet_impl(__attribute__((address_space(1))) float*, float) asm("llvm.amdgcn.global.atomic.fadd.p1f32.f32");
+static inline __device__ float atomicAddNoRet_impl(__attribute__((address_space(1))) float*, float) asm("llvm.amdgcn.global.atomic.fadd.f32.p1f32.f32");
 static inline __device__ void gpuAtomicAddNoReturn(float* address, float val) {
     using FP = __attribute__((address_space(0))) float*;
     using GP = __attribute__((address_space(1))) float*;
