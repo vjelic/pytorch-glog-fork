19d1cccc9a223e8c41b9c154c947a1f1b7d7a964
Jeff Daily <jeff.daily@amd.com>
2021-02-04 15:36:02 -0800

    remove gpuAtomicAddNoReturn and atomicAddNoReturn

--- aten/src/ATen/native/cuda/Indexing.cu
+++ aten/src/ATen/native/cuda/Indexing.cu
@@ -339,7 +339,7 @@ __global__ void indexAddSmallIndex(cuda::detail::TensorInfo<T, IndexType> dst,
           cuda::detail::IndexToOffset<T, IndexType, SrcDim>::get(linearIndex, src);
       srcOffset += srcIndex * src.strides[srcAddDim];
 
-      gpuAtomicAddNoReturn(&dst.data[dstOffset], alpha * src.data[srcOffset]);
+      gpuAtomicAdd(&dst.data[dstOffset], alpha * src.data[srcOffset]);
     }
   }
 }
@@ -389,7 +389,7 @@ __global__ void indexAddLargeIndex(cuda::detail::TensorInfo<T, IndexType> dst,
       cuda::detail::IndexToOffset<T, IndexType, SrcDim>::get(elementInSlice, src);
     srcOffset += srcIndex * src.strides[srcAddDim];
 
-    gpuAtomicAddNoReturn(&dst.data[dstOffset], alpha * src.data[srcOffset]);
+    gpuAtomicAdd(&dst.data[dstOffset], alpha * src.data[srcOffset]);
   }
 }
 
