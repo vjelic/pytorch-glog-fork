ba490b680eab8ef18a9940808af2980cd02ee593
Jeff Daily <jeff.daily@amd.com>
2021-02-16 23:59:36 +0000

    Revert "Allow ROCm CI to use non-default stream. (#48424)"
    This reverts commit d5c4a80cfdc0b253c43d0a198f26abec9e35cdce.

--- aten/src/ATen/miopen/Handle.cpp
+++ aten/src/ATen/miopen/Handle.cpp
@@ -1,53 +1,39 @@
-#include <ATen/miopen/Exceptions.h>
 #include <ATen/miopen/Handle.h>
-#include <ATen/hip/detail/DeviceThreadHandles.h>
-#include <c10/hip/HIPStream.h>
+
+#include <ATen/miopen/Exceptions.h>
+
+#include <unordered_map>
+#include <mutex>
 
 namespace at { namespace native {
+
 namespace {
 
-void createMIOpenHandle(miopenHandle_t *handle) {
-  MIOPEN_CHECK(miopenCreate(handle));
-}
+struct Handle {
+  miopenHandle_t handle;
+  Handle() : handle(NULL) {
+    MIOPEN_CHECK(miopenCreate(&handle));
+  }
+  ~Handle() {
+    if (handle) {
+      miopenDestroy(handle);
+    }
+  }
+};
 
-void destroyMIOpenHandle(miopenHandle_t handle) {
-// this is because of something dumb in the ordering of
-// destruction. Sometimes atexit, the cuda context (or something)
-// would already be destroyed by the time this gets destroyed. It
-// happens in fbcode setting. @colesbury and I decided to not destroy
-// the handle as a workaround.
-//   - @soumith
-//
-// Further note: this is now disabled globally, because we are seeing
-// the same issue as mentioned above in CUDA 11 CI.
-//   - @zasdfgbnm
-//
-// #ifdef NO_MIOPEN_DESTROY_HANDLE
-// #else
-//   miopenDestroy(handle);
-// #endif
-}
+std::mutex mutex;
+std::unordered_map<int, Handle> handles;
 
-using MIOpenPoolType = at::cuda::DeviceThreadHandlePool<miopenHandle_t, createMIOpenHandle, destroyMIOpenHandle>;
+}  // namespace
 
-} // namespace
 
-miopenHandle_t getMiopenHandle() {
+miopenHandle_t getMiopenHandle()
+{
   int device;
   HIP_CHECK(hipGetDevice(&device));
 
-  // Thread local PoolWindows are lazily-initialized
-  // to avoid initialization issues that caused hangs on Windows.
-  // See: https://github.com/pytorch/pytorch/pull/22405
-  // This thread local unique_ptrs will be destroyed when the thread terminates,
-  // releasing its reserved handles back to the pool.
-  static auto pool = std::make_shared<MIOpenPoolType>();
-  thread_local std::unique_ptr<MIOpenPoolType::PoolWindow> myPoolWindow(
-      pool->newPoolWindow());
-
-  auto handle = myPoolWindow->reserve(device);
-  MIOPEN_CHECK(miopenSetStream(handle, at::hip::getCurrentHIPStream()));
-  return handle;
+  std::lock_guard<std::mutex> guard(mutex);
+  return handles[device].handle;
 }
 
 }} // namespace at::native
