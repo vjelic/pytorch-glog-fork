03c639397bc063c5b3936a48293780967cf4fc29
Michael Melesse <micmelesse@gmail.com>
2021-03-04 11:29:20 -0800

    update run script

--- scripts/amd/docker_run_amd.sh
+++ scripts/amd/docker_run_amd.sh
@@ -9,7 +9,9 @@ WORK_DIR='/root/pytorch'
 
 # IMAGE_NAME=rocm/pytorch
 # IMAGE_NAME=rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch
-IMAGE_NAME=rocm/pytorch:rocm4.0.1_ubuntu18.04_py3.6_pytorch
+# IMAGE_NAME=rocm/pytorch:rocm4.0.1_ubuntu18.04_py3.6_pytorch
+# IMAGE_NAME=compute-artifactory.amd.com:5000/rocm-plus-docker/framework/compute-rocm-rel-4.1:11_ubuntu18.04_py3.6_pytorch_rocm4.1_internal_testing_5ce11d0_14
+IMAGE_NAME=rocm/pytorch-private:rocm4.1_internal_testing_hipfft_fix
 
 drun -d --name pytorch_container -w $WORK_DIR $VOLUMES $IMAGE_NAME
 docker cp . pytorch_container:/root/pytorch
