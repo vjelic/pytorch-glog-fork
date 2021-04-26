98a3d8515d1e8035d02594a2418f37d6b5c0e3a3
Jeff Daily <jeff.daily@amd.com>
2021-04-26 16:27:05 -0700

    remove use of hipMemcpyWithStream

--- aten/src/ATen/native/cuda/CUDAScalar.cu
+++ aten/src/ATen/native/cuda/CUDAScalar.cu
@@ -13,15 +13,6 @@ namespace native {
 
 Scalar _local_scalar_dense_cuda(const Tensor& self) {
   Scalar r;
-#if HIP_VERSION >= 301
-  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
-    at::ScalarType::Half, at::ScalarType::Bool, at::ScalarType::BFloat16, self.scalar_type(), "_local_scalar_dense_cuda", [&] {
-        scalar_t value;
-        cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-        AT_CUDA_CHECK(hipMemcpyWithStream(&value, self.data_ptr<scalar_t>(), sizeof(scalar_t), cudaMemcpyDeviceToHost, stream));
-        r = Scalar(value);
-      });
-#else
   AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
     at::ScalarType::Half, at::ScalarType::Bool, at::ScalarType::BFloat16, self.scalar_type(), "_local_scalar_dense_cuda", [&] {
         scalar_t value;
@@ -30,7 +21,6 @@ Scalar _local_scalar_dense_cuda(const Tensor& self) {
         AT_CUDA_CHECK(cudaStreamSynchronize(stream));
         r = Scalar(value);
       });
-#endif
   return r;
 }
 
