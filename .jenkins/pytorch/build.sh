01383c5ac3be0e78188132005e3b9e93c13c244c
Jithun Nair <jithun.nair@amd.com>
2021-07-22 23:18:48 +0000

    Set PYTORCH_ROCM_ARCH while building PyTorch

--- .jenkins/pytorch/build.sh
+++ .jenkins/pytorch/build.sh
@@ -152,6 +152,8 @@ if [[ "$BUILD_ENVIRONMENT" == *rocm* ]]; then
       # Set ROCM_ARCH to gfx900 and gfx906 for CI builds, if user doesn't override.
       echo "Limiting PYTORCH_ROCM_ARCH to gfx90[06] for CI builds"
       export PYTORCH_ROCM_ARCH="gfx900;gfx906"
+  else
+      export PYTORCH_ROCM_ARCH="gfx803;gfx900;gfx906;gfx908"
   fi
 
   python tools/amd_build/build_amd.py
