471a4f8bfea47971794b73900fb0e9a367f75db8
Juan Villamizar <100237675+jpvillam-amd@users.noreply.github.com>
2022-07-13 17:04:10 -0500

    [SOW MS3] test_sparse (#1034)
    * [ROCM] Enable some sparse tests on ROCM
    
    Enabling:
    test_sampled_addmm_errors_cuda_complex128
    test_sampled_addmm_errors_cuda_complex64
    test_sampled_addmm_errors_cuda_float32
    test_sampled_addmm_errors_cuda_float64
    test_sparse_add_cuda_complex128
    test_sparse_add_cuda_complex64
    
    * [ROCm] unskip
    
    * Hipified Sparse code to enable UTs
    
    * [ROCm] Enable sparse bmm tests
    
    * [ROCm] Enable test_csr_matvec
    
    * [ROCm} Cleanup some comments
    
    * [ROCm] re-adding cuda-hip mappings.
    
    * [ROCm] Added support for baddbmm and enabled test
    
    These changes require ROCm 5.2
    
    * [ROCm] More hipify and descriptors.

--- torch/testing/_internal/common_device_type.py
+++ torch/testing/_internal/common_device_type.py
@@ -1342,7 +1342,7 @@ def skipCUDAIfCudnnVersionLessThan(version=0):
 
 # Skips a test on CUDA if cuSparse generic API is not available
 def skipCUDAIfNoCusparseGeneric(fn):
-    return skipCUDAIf(not TEST_CUSPARSE_GENERIC, "cuSparse Generic API not available")(fn)
+    return skipCUDAIf(not TEST_WITH_ROCM and not TEST_CUSPARSE_GENERIC, "cuSparse Generic API not available")(fn)
 
 def skipCUDAIfNoHipsparseGeneric(fn):
     return skipCUDAIf(not TEST_HIPSPARSE_GENERIC, "hipSparse Generic API not available")(fn)
