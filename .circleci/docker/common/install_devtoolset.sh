c95b27c1e508805a9f8d479bc3fff79f90b1024c
rraminen <rraminen@amd.com>
2022-08-16 11:14:58 -0400

    Added scripts for centos and centos stream images

--- .circleci/docker/common/install_devtoolset.sh
+++ .circleci/docker/common/install_devtoolset.sh
@@ -4,9 +4,7 @@ set -ex
 
 [ -n "$DEVTOOLSET_VERSION" ]
 
-#yum install -y centos-release-scl
-#yum install -y devtoolset-$DEVTOOLSET_VERSION
+yum install -y centos-release-scl
+yum install -y devtoolset-$DEVTOOLSET_VERSION
 
-dnf install -y rpmdevtools
-
-#echo "source scl_source enable devtoolset-$DEVTOOLSET_VERSION" > "/etc/profile.d/devtoolset-$DEVTOOLSET_VERSION.sh"
+echo "source scl_source enable devtoolset-$DEVTOOLSET_VERSION" > "/etc/profile.d/devtoolset-$DEVTOOLSET_VERSION.sh"
