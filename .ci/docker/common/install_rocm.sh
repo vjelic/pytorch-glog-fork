e023400935d01e065c8ac5e6d64a9063b7b92590
Ramya Ramineni <62723901+rraminen@users.noreply.github.com>
2022-08-29 23:26:36 -0500

    [SOW MS3] Centos stream9 PyTorch image support (#1090)
    * changes to build Centos stream 9 images
    
    * Added scripts for centos and centos stream images
    
    * Added an extra line
    
    * Add ninja installation
    
    * Optimized code
    
    * Fixes
    
    * Add comment
    
    * Optimized code
    
    * Added AMDGPU mapping for ROCm 5.2 and invalid-url for rocm_baseurl
    
    Co-authored-by: Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>

--- .ci/docker/common/install_rocm.sh
+++ .ci/docker/common/install_rocm.sh
@@ -72,10 +72,16 @@ install_centos() {
   yum update -y
   yum install -y kmod
   yum install -y wget
-  yum install -y openblas-devel
+  
+  if [[ $OS_VERSION == 9 ]]; then 
+      dnf install -y openblas-serial
+      dnf install -y dkms kernel-headers kernel-devel
+  else
+      yum install -y openblas-devel
+      yum install -y dkms kernel-headers-`uname -r` kernel-devel-`uname -r`
+  fi
 
   yum install -y epel-release
-  yum install -y dkms kernel-headers-`uname -r` kernel-devel-`uname -r`
 
   # Add amdgpu repository
   local amdgpu_baseurl
@@ -91,7 +97,11 @@ install_centos() {
   echo "gpgcheck=1" >> /etc/yum.repos.d/amdgpu.repo
   echo "gpgkey=http://repo.radeon.com/rocm/rocm.gpg.key" >> /etc/yum.repos.d/amdgpu.repo
 
-  local rocm_baseurl="http://repo.radeon.com/rocm/yum/${ROCM_VERSION}"
+  if [[ $OS_VERSION == 9 ]]; then
+      local rocm_baseurl="invalid-url"
+  else
+      local rocm_baseurl="http://repo.radeon.com/rocm/yum/${ROCM_VERSION}/main"
+  fi
   echo "[ROCm]" > /etc/yum.repos.d/rocm.repo
   echo "name=ROCm" >> /etc/yum.repos.d/rocm.repo
   echo "baseurl=${rocm_baseurl}" >> /etc/yum.repos.d/rocm.repo
@@ -99,9 +109,13 @@ install_centos() {
   echo "gpgcheck=1" >> /etc/yum.repos.d/rocm.repo
   echo "gpgkey=http://repo.radeon.com/rocm/rocm.gpg.key" >> /etc/yum.repos.d/rocm.repo
 
-  yum update -y
-
-  yum install -y \
+  if [[ $OS_VERSION == 9 ]]; then
+      yum update -y --nogpgcheck
+      dnf --enablerepo=crb install -y perl-File-BaseDir
+      yum install -y --nogpgcheck rocm-ml-sdk rocm-developer-tools
+  else
+      yum update -y
+      yum install -y \
                    rocm-dev \
                    rocm-utils \
                    rocm-libs \
@@ -131,6 +145,8 @@ install_centos() {
   rm -rf /var/lib/yum/yumdb
   rm -rf /var/lib/yum/history
 }
+
+OS_VERSION=$(grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"')
 
 # Install Python packages depending on the base OS
 ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
