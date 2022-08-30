5cc86068275a84ddde04cdd07dcb3fe346f36721
Ramya Ramineni <62723901+rraminen@users.noreply.github.com>
2022-08-29 23:26:36 -0500

    [SOW MS3] Centos stream9 PyTorch image support (#1090)
    * changes to build Centos stream 9 images
    
    * Added scripts for centos and centos stream images
    
    * Added an extra line
    
    * Add ninja installation
    
    * Optimized code
    
    * Fixes
    
    * Add comment
    
    * Optimized code
    
    * Added AMDGPU mapping for ROCm 5.2 and invalid-url for rocm_baseurl
    
    Co-authored-by: Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>

--- .circleci/docker/build.sh
+++ .circleci/docker/build.sh
@@ -329,6 +329,12 @@ else
  DOCKERFILE_NAME="Dockerfile"
 fi
 
+if [[ "$image" == *centos9* ]]; then
+ DOCKERFILE_NAME="Dockerfile.centos.stream"
+else
+ DOCKERFILE_NAME="Dockerfile"
+fi
+
 # Build image
 # TODO: build-arg THRIFT is not turned on for any image, remove it once we confirm
 # it's no longer needed.
@@ -365,6 +371,7 @@ docker build \
        --build-arg "KATEX=${KATEX:-}" \
        --build-arg "ROCM_VERSION=${ROCM_VERSION:-}" \
        --build-arg "PYTORCH_ROCM_ARCH=${PYTORCH_ROCM_ARCH:-gfx900;gfx906;gfx908}" \
+       --build-arg "IMAGE_NAME=${IMAGE_NAME}" \
        -f $(dirname ${DOCKERFILE})/${DOCKERFILE_NAME} \
        -t "$tmp_tag" \
        "$@" \
