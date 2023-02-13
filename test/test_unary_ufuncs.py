bb0bf023242b348a643237a77bae0f9bc475c7e2
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2023-02-13 12:30:53 -0800

    [ROCm]: Adjust tolerance level for reciprocal test (#1179)
    Refer #884
    
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- test/test_unary_ufuncs.py
+++ test/test_unary_ufuncs.py
@@ -498,7 +498,8 @@ class TestUnaryUfuncs(TestCase):
         else:
             res = op(input, out=output, **kwargs)
             self.assertTrue(res is output)
-            if all([TEST_WITH_ROCM, op.name == "sinc", expected.dtype == torch.complex64, output.dtype == torch.complex128]):
+            if ( all([TEST_WITH_ROCM, op.name == "sinc", expected.dtype == torch.complex64, output.dtype == torch.complex128]) or
+                 all([TEST_WITH_ROCM, op.name == "reciprocal", expected.dtype == torch.complex64, output.dtype == torch.complex128]) ):
                 self.assertEqual(output, expected.to(output.dtype), atol=1e-6, rtol=1e-6)
             else:
                 self.assertEqual(output, expected.to(output.dtype))
