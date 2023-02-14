876a23bd28d470183befd68107108f32445683e8
Jack Taylor <jack.taylor@amd.com>
2023-02-14 11:20:00 +0000

    Use BUILD_ENVIRONMENT to check rocm for install_triton

--- .ci/pytorch/common_utils.sh
+++ .ci/pytorch/common_utils.sh
@@ -134,8 +134,8 @@ function install_filelock() {
 }
 
 function install_triton() {
-  local commit
-  if [[ "${TEST_CONFIG}" == *rocm* ]]; then
+  local commit 
+  if [[ "${BUILD_ENVIRONMENT}" == *rocm* ]]; then
     echo "skipping triton due to rocm"
   else
     commit=$(get_pinned_commit triton)
