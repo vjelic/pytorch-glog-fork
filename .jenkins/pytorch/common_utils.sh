2847085285d93eaf5884322585655e3ba949ab4a
Pruthvi Madugundu <pruthvigithub@gmail.com>
2023-02-14 22:48:14 -0800

    Skiping install of triton in test scripts for ROCm

--- .jenkins/pytorch/common_utils.sh
+++ .jenkins/pytorch/common_utils.sh
@@ -148,7 +148,7 @@ function install_filelock() {
 
 function install_triton() {
   local commit
-  if [[ "${TEST_CONFIG}" == *rocm* ]]; then
+  if [[ "${BUILD_ENVIRONMENT}" == *rocm* ]]; then
     echo "skipping triton due to rocm"
   else
     commit=$(get_pinned_commit triton)
