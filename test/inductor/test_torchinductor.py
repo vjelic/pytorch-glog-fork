2d00d6f60e6c0dc150508475ad85114b77cbe6e7
Jack Taylor <108682042+jataylo@users.noreply.github.com>
2023-02-21 17:07:08 +0000

    Additional UT skips/unskips for ROCm
    Makes skips/unskips for dynamo and inductor UTs.

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -2467,6 +2467,8 @@ class CommonTemplate:
 
         self.common(fn, (torch.randn(4), torch.randn(4)), check_lowp=False)
 
+    # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3464
+    @skipIfRocm
     @requires_multigpu()
     def test_recompile_on_index(self):
         torch.set_float32_matmul_precision("high")
@@ -2506,6 +2508,7 @@ class CommonTemplate:
             (torch.randn([4, 4, 4]),),
         )
 
+    # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3462
     @skipIfRocm
     def test_convolution1(self):
         m = torch.nn.Sequential(
@@ -2967,6 +2970,7 @@ class CommonTemplate:
             (torch.randn([10, 4]), torch.randint(10, [8]), torch.tensor([0, 2, 6])),
         )
 
+    # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3597
     @skipIfRocm
     def test_batch_norm_2d(self):
         m = torch.nn.Sequential(
@@ -3656,6 +3660,7 @@ class CommonTemplate:
                 ),
             )
 
+    # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3465
     @skipIfRocm
     def test_cudnn_rnn(self):
         if self.device == "cpu":
@@ -4073,7 +4078,7 @@ class CommonTemplate:
         )
         self.assertTrue(same(fn(*inputs), inputs[0] + inputs[1]))
 
-    @config.patch({"triton.cudagraphs": True if not torch.version.hip else False})
+    @config.patch({"triton.cudagraphs": True})
     @patch.object(functorch_config, "use_fake_tensor", True)
     def test_input_mutation1(self):
         def fn(a):
@@ -4614,6 +4619,7 @@ class CommonTemplate:
             ],
         )
 
+    # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3378
     @skipIfRocm
     def test_scatter3(self):
         def fn(a, dim, index, b):
@@ -4658,6 +4664,7 @@ class CommonTemplate:
             ],
         )
 
+    # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3378
     @skipIfRocm
     def test_scatter_add2(self):
         def fn(a, dim, index, b):
@@ -4673,6 +4680,7 @@ class CommonTemplate:
             ],
         )
 
+    # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3378
     @skipIfRocm
     def test_scatter_add3(self):
         def fn(a, dim, index, b):
@@ -4688,6 +4696,7 @@ class CommonTemplate:
             ],
         )
 
+    # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3378
     @skipIfRocm
     def test_scatter_reduce1(self):
         def fn(a, dim, index, b):
@@ -4703,6 +4712,7 @@ class CommonTemplate:
             ],
         )
 
+    # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3378
     @skipIfRocm
     def test_scatter_reduce2(self):
         def fn(a, dim, index, b):
@@ -5222,6 +5232,7 @@ class CommonTemplate:
             ],
         )
 
+    # FIXME: Tensors are not alike https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3462
     @skipIfRocm
     def test_argmax_argmin2(self):
         def fn(x):
@@ -6885,6 +6896,7 @@ if HAS_CUDA and not TEST_WITH_ASAN:
     class CudaReproTests(TestCase):
         common = check_model_cuda
 
+        # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3387
         @skipIfRocm
         def test_index_put_issue(self):
             def forward(
@@ -6922,7 +6934,6 @@ if HAS_CUDA and not TEST_WITH_ASAN:
             compiled = compile_fx_inner(mod, inps)
             compiled(inps)
 
-        @skipIfRocm
         @requires_cuda()
         def test_input_channels_last(self):
             m = torch.nn.Sequential(
@@ -6994,7 +7005,7 @@ if HAS_CUDA and not TEST_WITH_ASAN:
             compiled = compile_fx_inner(mod, ())
             assert compiled([])[0].device.type == "cuda"
 
-        @config.patch({"triton.cudagraphs": True if not torch.version.hip else False})
+        @config.patch({"triton.cudagraphs": True})
         def test_expanded_inputs_cudagraphs(self):
             @torch._dynamo.optimize("inductor")
             def fn(x, y):
@@ -7028,7 +7039,7 @@ if HAS_CUDA and not TEST_WITH_ASAN:
             self.assertEqual(real_out, compiled_out)
             torch._dynamo.reset()
 
-        @config.patch({"triton.cudagraphs": True if not torch.version.hip else False, "size_asserts": False})
+        @config.patch({"triton.cudagraphs": True})
         def test_expanded_inputs_cudagraphs_no_size_asserts(self):
             @torch._dynamo.optimize("inductor")
             def fn(x, y):
@@ -7071,6 +7082,7 @@ if HAS_CUDA and not TEST_WITH_ASAN:
             res = opt_mod(*args)
             self.assertTrue(same(ref, res))
 
+        # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3462
         @skipIfRocm
         @config.patch({"triton.cudagraphs": True if not torch.version.hip else False})
         def test_inplace_updates_cudagraphs(self):
@@ -7353,6 +7365,7 @@ if HAS_CUDA and not TEST_WITH_ASAN:
             self.assertEqual(arguments_that_are_divisible_by_16_in_kernel1, (0, 1))
             torch._dynamo.reset()
 
+        # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3463
         @skipIfRocm
         def test_optimize_indexing_dtype(self):
             def fn(x: torch.Tensor) -> torch.Tensor:
@@ -7366,6 +7379,8 @@ if HAS_CUDA and not TEST_WITH_ASAN:
 
             self.assertEqual(fn_opt(*inps), fn(*inps))
 
+        # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3463
+        @skipIfRocm
         def test_not_materialize_pointwise_reduction(self):
             def fn(a, b):
                 return (a - b).sum(dim=-1).amax(dim=-1)
@@ -7383,6 +7398,7 @@ if HAS_CUDA and not TEST_WITH_ASAN:
             self.assertFalse("out_ptr0" in code)
             self.assertEqual(fn_opt(*inps), fn(*inps))
 
+        # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3463
         @skipIfRocm
         def test_cant_optimize_compute(self):
             def ones():
@@ -7410,6 +7426,7 @@ if HAS_CUDA and not TEST_WITH_ASAN:
                 self.assertTrue("to(tl.int64)" in code)
                 self.assertEqual(fn_opt(), fn())
 
+        # FIXME: https://github.com/ROCmSoftwarePlatform/frameworks-internal/issues/3463
         @skipIfRocm
         def test_optimize_compute(self):
             def ones():
