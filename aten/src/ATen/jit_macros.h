305e9ef04b50ed489fa88cfba13517cd350e3e80
Jeff Daily <jeff.daily@amd.com>
2023-03-22 16:14:16 +0000

    disable jiterator

--- aten/src/ATen/jit_macros.h
+++ aten/src/ATen/jit_macros.h
@@ -3,5 +3,5 @@
 #include <string>
 
 // AT_USE_JITERATOR(), controls whether we jit some elementwise kernels
-#define AT_USE_JITERATOR() true
+#define AT_USE_JITERATOR() false
 #define jiterator_stringify(...) std::string(#__VA_ARGS__);
