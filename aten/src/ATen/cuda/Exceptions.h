fd5c8dff821599ca4ad69061c5a677b2b966e3ea
Zachary Streeter <Zachary.Streeter@amd.com>
2023-06-12 18:58:56 +0000

    hipblaslt initial integration

--- aten/src/ATen/cuda/Exceptions.h
+++ aten/src/ATen/cuda/Exceptions.h
@@ -50,6 +50,9 @@ class CuDNNError : public c10::Error {
 
 namespace at { namespace cuda { namespace blas {
 C10_EXPORT const char* _cublasGetErrorEnum(cublasStatus_t error);
+#ifdef USE_ROCM
+C10_EXPORT const char* _hipblasGetErrorEnum(hipblasStatus_t error);
+#endif
 }}} // namespace at::cuda::blas
 
 
