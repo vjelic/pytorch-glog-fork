798f5328b76ed9b1d0caf0f0fdf682496ab0091d
Jeff Daily <jeff.daily@amd.com>
2023-07-18 21:23:08 +0000

    disable jiterator when USE_ASAN is defined

--- aten/src/ATen/jit_macros.h
+++ aten/src/ATen/jit_macros.h
@@ -3,5 +3,9 @@
 #include <string>
 
 // AT_USE_JITERATOR(), controls whether we jit some elementwise kernels
+#if defined(USE_ROCM) && defined(USE_ASAN)
+#define AT_USE_JITERATOR() false
+#else
 #define AT_USE_JITERATOR() true
+#endif
 #define jiterator_stringify(...) std::string(#__VA_ARGS__);
