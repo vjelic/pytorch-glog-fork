bdf2207f8d22513c26961db0322c2209e5c04b95
Pruthvi Madugundu <pruthvigithub@gmail.com>
2023-09-25 16:29:28 -0700

    Changes to support docker v23
    Reversed the condition as required

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -348,10 +348,21 @@ if [[ "$image" == *cuda*  && ${OS} == "ubuntu" ]]; then
   fi
 fi
 
+if [[ "$image" == *centos9* ]]; then
+ DOCKERFILE_NAME="Dockerfile.centos.stream"
+else
+ DOCKERFILE_NAME="Dockerfile"
+fi
+
+DOCKER_PROGRESS="--progress=plain"
+if [[ "${DOCKER_BUILDKIT}" == 0 ]]; then
+  DOCKER_PROGRESS=""
+fi
+
 # Build image
 docker build \
        --no-cache \
-       --progress=plain \
+       ${DOCKER_PROGRESS} \
        --build-arg "BUILD_ENVIRONMENT=${image}" \
        --build-arg "PROTOBUF=${PROTOBUF:-}" \
        --build-arg "LLVMDEV=${LLVMDEV:-}" \
@@ -387,7 +398,7 @@ docker build \
        --build-arg "INDUCTOR_BENCHMARKS=${INDUCTOR_BENCHMARKS}" \
        --build-arg "EXECUTORCH=${EXECUTORCH}" \
        --build-arg "BASEKIT_VERSION=${BASEKIT_VERSION}" \
-       -f $(dirname ${DOCKERFILE})/Dockerfile \
+       -f $(dirname ${DOCKERFILE})/${DOCKERFILE_NAME} \
        -t "$tmp_tag" \
        "$@" \
        .
