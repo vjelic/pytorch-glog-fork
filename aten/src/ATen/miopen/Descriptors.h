123bfd1e530dd77933dd06bbb53f3cd1baea73dd
Jeff Daily <jeff.daily@amd.com>
2023-10-10 22:24:28 +0000

    Revert "[ROCm] Add miopen determinism support for convolutions (#107028)"
    This reverts commit 19db570cd9906777742c27625001ec07c4b4b9e4.

--- aten/src/ATen/miopen/Descriptors.h
+++ aten/src/ATen/miopen/Descriptors.h
@@ -111,10 +111,9 @@ struct ConvolutionDescriptor
                       &miopenCreateConvolutionDescriptor,
                       &miopenDestroyConvolutionDescriptor>
 {
-  void set(miopenDataType_t dataType, miopenConvolutionMode_t c_mode,  int dim, int* pad, int* stride, int * upscale /* aka dilation */, int groups, bool deterministic) {
+  void set(miopenDataType_t dataType, miopenConvolutionMode_t c_mode,  int dim, int* pad, int* stride, int * upscale /* aka dilation */, int groups) {
     MIOPEN_CHECK(miopenInitConvolutionNdDescriptor(mut_desc(), dim, pad, stride, upscale, c_mode));
     MIOPEN_CHECK(miopenSetConvolutionGroupCount(mut_desc(), groups));
-    MIOPEN_CHECK(miopenSetConvolutionAttribute(mut_desc(), MIOPEN_CONVOLUTION_ATTRIB_DETERMINISTIC, deterministic ? 1 : 0));
   }
 };
 
