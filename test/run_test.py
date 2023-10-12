e2bad706154406e5456e9488f6c9b631a254b0d5
Bo Li <110066325+BLOrange-AMD@users.noreply.github.com>
2023-10-12 16:39:00 -0500

    Include test_vmap into rocm_blocklist (#1296)

--- test/run_test.py
+++ test/run_test.py
@@ -290,6 +290,7 @@ ROCM_BLOCKLIST = [
     "test_jit_legacy",
     "test_cuda_nvml_based_avail",
     "test_jit_cuda_fuser", # Skipped until NVFuser enabled - https://ontrack-internal.amd.com/browse/SWDEV-361875
+    "functorch/test_vmap",
 ]
 
 # The tests inside these files should never be run in parallel with each other
