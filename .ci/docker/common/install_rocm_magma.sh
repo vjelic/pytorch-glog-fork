9518fb6cc50b2f73c51107acee506e595b568e16
Pruthvi Madugundu <pruthvigithub@gmail.com>
2023-11-02 00:17:38 -0700

    gcnArch hip deprecation - magma change

--- .ci/docker/common/install_rocm_magma.sh
+++ .ci/docker/common/install_rocm_magma.sh
@@ -3,11 +3,12 @@
 set -ex
 
 # "install" hipMAGMA into /opt/rocm/magma by copying after build
-git clone https://bitbucket.org/icl/magma.git
+# Moved to temp fork SWDEV-429841
+git clone https://bitbucket.org/mpruthvi1/magma.git -b rocm60_gcn_depr
 pushd magma
 
 # Version 2.7.2 + ROCm related updates
-git checkout 823531632140d0edcb7e77c3edc0e837421471c5
+git checkout 825f861ae834407946fb748834e4e025ac7d7064
 
 cp make.inc-examples/make.inc.hip-gcc-mkl make.inc
 echo 'LIBDIR += -L$(MKLROOT)/lib' >> make.inc
