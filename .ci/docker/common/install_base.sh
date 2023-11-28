7857d93778b76968cf4baaef205c375ef80af621
Pruthvi Madugundu <pruthvigithub@gmail.com>
2023-11-28 00:35:34 -0800

    [CS9] Updates to CentOS stream 9 build (#1326)
    - Add missing common_utils.sh
    - Update the install vision part
    - Move to amdgpu rhel 9.3 builds
    - Update to pick python from conda path
    - Add a missing package
    - Add ROCM_PATH and magma
    - Updated repo radeon path

--- .ci/docker/common/install_base.sh
+++ .ci/docker/common/install_base.sh
@@ -161,6 +161,7 @@ install_centos() {
   if [[ $OS_VERSION == 9 ]]
   then
 	  dnf --enablerepo=crb -y install libsndfile-devel
+	  yum install -y procps
   else
 	  yum install -y \
             opencv-devel \
