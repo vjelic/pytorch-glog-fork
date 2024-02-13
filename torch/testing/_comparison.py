a621985256e768eadf5abf8f6e28a45d547d6453
Xinya Zhang <Xinya.Zhang@amd.com>
2024-02-13 00:22:08 +0000

    Revert "[To Drop?] print actual and expected values for greatest difference elements when tensor mismatches."
    This reverts commit befb73f2c514aa35cbe3fa86a0cee4311d605652.

--- torch/testing/_comparison.py
+++ torch/testing/_comparison.py
@@ -153,8 +153,6 @@ def _make_mismatch_msg(
     rel_diff: float,
     rel_diff_idx: Optional[Union[int, Tuple[int, ...]]] = None,
     rtol: float,
-    actual: Optional[torch.Tensor] = None,
-    expected: Optional[torch.Tensor] = None,
 ) -> str:
     """Makes a mismatch error message for numeric values.
 
@@ -181,8 +179,6 @@ def _make_mismatch_msg(
         diff: float,
         idx: Optional[Union[int, Tuple[int, ...]]],
         tol: float,
-        actual: Optional[torch.Tensor] = None,
-        expected: Optional[torch.Tensor] = None,
     ) -> str:
         if idx is None:
             msg = f"{type.title()} difference: {diff}"
@@ -190,8 +186,6 @@ def _make_mismatch_msg(
             msg = f"Greatest {type} difference: {diff} at index {idx}"
         if not equality:
             msg += f" (up to {tol} allowed)"
-        if actual is not None and expected is not None:
-            msg += f' actual value {actual[idx]}, expected {expected[idx]}'
         return msg + "\n"
 
     if identifier is None:
@@ -204,8 +198,8 @@ def _make_mismatch_msg(
     if extra:
         msg += f"{extra.strip()}\n"
 
-    msg += make_diff_msg(type="absolute", diff=abs_diff, idx=abs_diff_idx, tol=atol, actual=actual, expected=expected)
-    msg += make_diff_msg(type="relative", diff=rel_diff, idx=rel_diff_idx, tol=rtol, actual=actual, expected=expected)
+    msg += make_diff_msg(type="absolute", diff=abs_diff, idx=abs_diff_idx, tol=atol)
+    msg += make_diff_msg(type="relative", diff=rel_diff, idx=rel_diff_idx, tol=rtol)
 
     return msg.strip()
 
@@ -313,8 +307,6 @@ def make_tensor_mismatch_msg(
         rel_diff=max_rel_diff.item(),
         rel_diff_idx=unravel_flat_index(int(max_rel_diff_flat_idx)),
         rtol=rtol,
-        actual=actual,
-        expected=expected,
     )
 
 
