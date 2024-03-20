43104efe3920e9b871119974ae7c0971af643b30
Xinya Zhang <Xinya.Zhang@amd.com>
2024-03-20 06:29:13 +0000

    Use pre-compiled aotriton due to the complexity of its build system.

--- cmake/External/aotriton.cmake
+++ cmake/External/aotriton.cmake
@@ -1,28 +1,29 @@
 if(NOT __AOTRITON_INCLUDED)
   set(__AOTRITON_INCLUDED TRUE)
 
-  set(__AOTRITON_SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/aotriton/src")
-  set(__AOTRITON_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/aotriton/build")
-  set(__AOTRITON_INSTALL_DIR "${PROJECT_SOURCE_DIR}/torch")
+  set(__AOTRITON_TARBALL_TAG "0.4b")
+  if(ROCM_VERSION_DEV VERSION_GREATER_EQUAL "6.0.0")
+    set(__AOTRITON_TARBALL_PLATFORM "rocm6")
+  else()
+    set(__AOTRITON_TARBALL_PLATFORM "rocm5")
+  endif()
+  string(CONCAT __AOTRITON_TARBALL_URL
+    "https://github.com/ROCm/aotriton/releases/download/"
+    "${__AOTRITON_TARBALL_TAG}/"
+    "aotriton-${__AOTRITON_TARBALL_TAG}"
+    "-manylinux2014_x86_64-"
+    "${__AOTRITON_TARBALL_PLATFORM}.tar.bz2")
   ExternalProject_Add(aotriton_external
-    GIT_REPOSITORY https://github.com/ROCm/aotriton.git
-    GIT_TAG 5d9a1dbcf5b17ff798ff77f60a8a08fa41953ff0
-    SOURCE_DIR ${__AOTRITON_SOURCE_DIR}
-    BINARY_DIR ${__AOTRITON_BUILD_DIR}
-    PREFIX ${__AOTRITON_INSTALL_DIR}
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${__AOTRITON_INSTALL_DIR}
-    -DAOTRITON_COMPRESS_KERNEL=OFF
-    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-    -DAOTRITON_NO_PYTHON=ON
-    -DAOTRITON_NO_SHARED=ON
-    # CONFIGURE_COMMAND ""
-    # BUILD_COMMAND ${MAKE_COMMAND}
-    BUILD_BYPRODUCTS "${__AOTRITON_INSTALL_DIR}/lib/libaotriton_v2.a"
-    # INSTALL_COMMAND ${MAKE_COMMAND} install
-    )
+    URL ${__AOTRITON_TARBALL_URL}
+    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/aotriton_tarball
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    BUILD_BYPRODUCTS ${CMAKE_CURRENT_BINARY_DIR}/aotriton_tarball/lib/libaotriton_v2.a
+  )
   set(AOTRITON_FOUND TRUE)
   add_library(__caffe2_aotriton INTERFACE)
   add_dependencies(__caffe2_aotriton aotriton_external)
-  target_link_libraries(__caffe2_aotriton INTERFACE ${__AOTRITON_INSTALL_DIR}/lib/libaotriton_v2.a)
-  target_include_directories(__caffe2_aotriton INTERFACE ${__AOTRITON_INSTALL_DIR}/include)
+  target_link_libraries(__caffe2_aotriton INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/aotriton_tarball/lib/libaotriton_v2.a)
+  target_include_directories(__caffe2_aotriton INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/aotriton_tarball/include)
 endif() # __AOTRITON_INCLUDED
