eb3a9e3824d3aced496447fb0f8b6103c5190706
root <root@banff-sc-cs47-38.dh170.dcgpu>
2024-03-29 03:58:11 +0000

    update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 git submodule update --init --recursive
 python3 tools/amd_build/build_amd.py
-PYTORCH_ROCM_ARCH=$1 ROCM_PATH=/opt/rocm/ CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"} BUILD_TEST=0 python3 setup.py install --user
+MAX_JOBS=128 PYTORCH_ROCM_ARCH=$1 ROCM_PATH=/opt/rocm/ CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"} python3 setup.py install
