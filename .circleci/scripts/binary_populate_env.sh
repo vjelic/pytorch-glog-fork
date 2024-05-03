284e232b90de4070ee1622bb7ee681d1f8789dd1
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2024-05-03 13:40:13 -0500

    [release/2.3] Update triton dependency (#1405)
    * Add ROCM_VERSION to triton dependency
    
    * Use DESIRED_CUDA env var as it's already defined in rocAutomation build_wheel.sh when starting docker container
    
    * Use DESIRED_CUDA in the correct location and remove condition on dev package naming

--- .circleci/scripts/binary_populate_env.sh
+++ .circleci/scripts/binary_populate_env.sh
@@ -91,9 +91,9 @@ fi
 # Set triton via PYTORCH_EXTRA_INSTALL_REQUIREMENTS for triton rocm package
 if [[ "$PACKAGE_TYPE" =~ .*wheel.* && -n "$PYTORCH_BUILD_VERSION" && "$PYTORCH_BUILD_VERSION" =~ .*rocm.* && $(uname) == "Linux" && "$DESIRED_PYTHON" != "3.12" ]]; then
     TRITON_REQUIREMENT="pytorch-triton-rocm==${TRITON_VERSION}"
-    if [[ -n "$PYTORCH_BUILD_VERSION" && "$PYTORCH_BUILD_VERSION" =~ .*dev.* ]]; then
+    if [[ -n "$PYTORCH_BUILD_VERSION" ]]; then
         TRITON_SHORTHASH=$(cut -c1-10 $PYTORCH_ROOT/.ci/docker/ci_commit_pins/triton-rocm.txt)
-        TRITON_REQUIREMENT="pytorch-triton-rocm==${TRITON_VERSION}+${TRITON_SHORTHASH}"
+        TRITON_REQUIREMENT="pytorch-triton-rocm==${TRITON_VERSION}+${DESIRED_CUDA}.${TRITON_SHORTHASH}"
     fi
     if [[ -z "${PYTORCH_EXTRA_INSTALL_REQUIREMENTS:-}" ]]; then
         export PYTORCH_EXTRA_INSTALL_REQUIREMENTS="${TRITON_REQUIREMENT}"
