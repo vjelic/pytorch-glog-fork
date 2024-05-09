beb6f825fe76e1a488344f05a55b09812db9d5ef
Dmitry Nikolaev <dmitry.nikolaev@amd.com>
2024-05-09 21:23:03 +0000

    use native nhwc batchnorm to verify miopen

--- aten/src/ATen/native/Normalization.cpp
+++ aten/src/ATen/native/Normalization.cpp
@@ -512,7 +512,8 @@ BatchNormBackend _select_batch_norm_backend(
 
   // TODO: Remove PYTORCH_MIOPEN_SUGGEST_NHWC once ROCm officially supports NHWC in MIOpen
   // See #64427
-  static bool PYTORCH_MIOPEN_SUGGEST_NHWC = c10::utils::check_env("PYTORCH_MIOPEN_SUGGEST_NHWC").value_or(false);
+  // non static variable is used to be able to change environment variable in runtime for testing
+  bool PYTORCH_MIOPEN_SUGGEST_NHWC = c10::utils::check_env("PYTORCH_MIOPEN_SUGGEST_NHWC").value_or(false);
 
   if (
       input.is_cuda()
