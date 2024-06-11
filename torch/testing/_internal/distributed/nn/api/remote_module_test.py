a390471ab968802b77b5ffe69a9f29089a379d79
Pruthvi Madugundu <pruthvigithub@gmail.com>
2024-06-11 10:59:31 -0700

    [HIP] Few more updates to the returned error string

--- torch/testing/_internal/distributed/nn/api/remote_module_test.py
+++ torch/testing/_internal/distributed/nn/api/remote_module_test.py
@@ -615,7 +615,7 @@ class CudaRemoteModuleTest(CommonRemoteModuleTest):
 
         if TEST_WITH_ROCM:
             errorString = (r"HIP error: invalid device ordinal\n"
-                          r"HIP kernel errors might be asynchronously reported at some other API call,so the stacktrace below might be incorrect.\n"
+                          r"HIP kernel errors might be asynchronously reported at some other API call, so the stacktrace below might be incorrect.\n"
                           r"For debugging consider passing AMD_SERIALIZE_KERNEL=3.")
         else:
             errorString = r"CUDA error: invalid device ordinal"
