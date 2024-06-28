4d3bd90442cae7bc9f22e30a28e03b6467dd81fd
Jack Taylor <108682042+jataylo@users.noreply.github.com>
2024-06-28 12:17:18 +0100

    Patching mistake in clock_info fix

--- torch/cuda/__init__.py
+++ torch/cuda/__init__.py
@@ -1045,7 +1045,7 @@ def _get_amdsmi_power_draw(device: Optional[Union[Device, int]] = None) -> int:
 def _get_amdsmi_clock_rate(device: Optional[Union[Device, int]] = None) -> int:
     handle = _get_amdsmi_handler(device)
     clock_info = amdsmi.amdsmi_get_clock_info(handle, amdsmi.AmdSmiClkType.GFX)
-    if "cur_clk" in clock:
+    if "cur_clk" in clock_info:
         return clock_info["cur_clk"]
     else:
         return clock_info["clk"]
