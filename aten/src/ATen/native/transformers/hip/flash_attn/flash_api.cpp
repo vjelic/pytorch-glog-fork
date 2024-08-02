a8b328beea4b576c39d9a4f025acc57ec37d97c1
Joseph Groenenboom <joseph.groenenboom@amd.com>
2024-08-02 19:16:33 +0000

    Match signatures, fix dropout.

--- aten/src/ATen/native/transformers/hip/flash_attn/flash_api.cpp
+++ aten/src/ATen/native/transformers/hip/flash_attn/flash_api.cpp
@@ -28,14 +28,14 @@ mha_fwd(const at::Tensor &q,
         c10::optional<at::Generator> gen_);
 
 std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor>
-mha_varlen_fwd(at::Tensor &q,                            // total_q x num_heads x head_size, total_q := \sum_{i=0}^{b} s_i
+mha_varlen_fwd(const at::Tensor &q,                            // total_q x num_heads x head_size, total_q := \sum_{i=0}^{b} s_i
                const at::Tensor &k,                      // total_k x num_heads_k x head_size, total_k := \sum_{i=0}^{b} s_i or num_blocks x page_block_size x num_heads_k x head_size if there's a block_table.
                const at::Tensor &v,                      // total_k x num_heads_k x head_size, total_k := \sum_{i=0}^{b} s_i or num_blocks x page_block_size x num_heads_k x head_size if there's a block_table.
                c10::optional<at::Tensor> &out_,          // total_q x num_heads x head_size, total_k := \sum_{i=0}^{b} s_i
                const at::Tensor &cu_seqlens_q,           // b+1
                const at::Tensor &cu_seqlens_k,           // b+1
                c10::optional<at::Tensor> &seqused_k,     // b. If given, only this many elements of each batch element's keys are used.
-               c10::optional<at::Tensor> &block_table_,  // batch_size x max_num_blocks_per_seq
+               //               c10::optional<at::Tensor> &block_table_,  // batch_size x max_num_blocks_per_seq
                c10::optional<at::Tensor> &alibi_slopes_, // num_heads or b x num_heads
                int max_seqlen_q,
                const int max_seqlen_k,
@@ -65,8 +65,8 @@ mha_bwd(const at::Tensor &dout,                   // batch_size x seqlen_q x num
         int window_size_left,
         int window_size_right,
         const bool deterministic,
-        c10::optional<at::Generator> gen_,
-        c10::optional<at::Tensor> &rng_state);
+        const at::Tensor philox_seed,
+        const at::Tensor philox_offset);
 
 std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor>
 mha_varlen_bwd(const at::Tensor &dout,                   // total_q x num_heads x head_size
@@ -90,9 +90,8 @@ mha_varlen_bwd(const at::Tensor &dout,                   // total_q x num_heads 
                int window_size_left,
                int window_size_right,
                const bool deterministic,
-               c10::optional<at::Generator> gen_,
-               c10::optional<at::Tensor> &rng_state);
-
+               const at::Tensor philox_seed,
+               const at::Tensor philox_offset);
 }
 
 // PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
