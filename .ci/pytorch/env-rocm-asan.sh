0eea2ae741840b298d1a25abc426fca18bba951b
Jeff Daily <jeff.daily@amd.com>
2024-08-06 23:49:56 +0000

    update env vars for ROCm 6.2

--- .ci/pytorch/env-rocm-asan.sh
+++ .ci/pytorch/env-rocm-asan.sh
@@ -8,7 +8,7 @@ export ASAN_OPTIONS=detect_leaks=0:detect_stack_use_after_return=1:symbolize=1:d
 export CMAKE_PREFIX_PATH=/opt/conda
 
 # otherwise any program run at build time will fail
-export LD_LIBRARY_PATH=/opt/rocm/llvm/lib/clang/17/lib/linux
+export LD_LIBRARY_PATH=/opt/rocm/llvm/lib/clang/18/lib/linux
 export HSA_XNACK=1
 
 # TODO: Make the ASAN flags a centralized env var and unify with USE_ASAN option
@@ -16,8 +16,8 @@ export CC="/opt/rocm/llvm/bin/clang"
 export CXX="/opt/rocm/llvm/bin/clang++"
 export LDSHARED="/opt/rocm/llvm/bin/clang --shared -fuse-ld=lld"
 export LDFLAGS="-fuse-ld=lld -fsanitize=address -shared-libasan -g"
-export CFLAGS="-g -fsanitize=address -shared-libasan -Wno-cast-function-type-strict"
-export CXXFLAGS="-g -fsanitize=address -shared-libasan -Wno-cast-function-type-strict"
+export CFLAGS="-g -fsanitize=address -shared-libasan -Wno-cast-function-type-strict -fclang-abi-compat=17"
+export CXXFLAGS="-g -fsanitize=address -shared-libasan -Wno-cast-function-type-strict -fclang-abi-compat=17"
 export USE_ASAN=1
 export USE_CUDA=0
 export USE_ROCM=1
@@ -26,6 +26,6 @@ export USE_MKLDNN=0
 # only add these env vars after build is completed
 if test "x$BUILD_ONLY_ENV_VARS" = x
 then
-    export LD_PRELOAD="/opt/rocm/llvm/lib/clang/17/lib/linux/libclang_rt.asan-x86_64.so"
-    export LD_LIBRARY_PATH="/opt/rocm/llvm/lib/clang/17/lib/linux:/opt/rocm/lib/asan"
+    export LD_PRELOAD="/opt/rocm/llvm/lib/clang/18/lib/linux/libclang_rt.asan-x86_64.so"
+    export LD_LIBRARY_PATH="/opt/rocm/llvm/lib/clang/18/lib/linux:/opt/rocm/lib/asan"
 fi
