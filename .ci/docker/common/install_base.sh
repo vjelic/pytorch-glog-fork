e26cd64fce2764d4e99fdb8e80981100d5ec4bc4
Pruthvi Madugundu <pruthvigithub@gmail.com>
2024-10-09 02:25:31 -0500

    Bump cmake version 3.28

--- .ci/docker/common/install_base.sh
+++ .ci/docker/common/install_base.sh
@@ -12,8 +12,11 @@ install_ubuntu() {
   if [[ "$UBUNTU_VERSION" == "20.04"* ]]; then
     cmake3="cmake=3.16*"
     maybe_libiomp_dev=""
-  elif [[ "$UBUNTU_VERSION" == "22.04"* ]] || [[ "$UBUNTU_VERSION" == "24.04"* ]] ; then
+  elif [[ "$UBUNTU_VERSION" == "22.04"* ]]; then
     cmake3="cmake=3.22*"
+    maybe_libiomp_dev=""
+  elif [[ "$UBUNTU_VERSION" == "24.04"* ]]; then
+    cmake3="cmake=3.28*"
     maybe_libiomp_dev=""
   else
     cmake3="cmake=3.5*"
