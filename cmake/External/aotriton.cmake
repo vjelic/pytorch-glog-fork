6edd36f39345e6d785349e34eb6571c9c915cda6
Xinya Zhang <Xinya.Zhang@amd.com>
2025-01-13 22:01:49 +0000

    Enable head_dim == 512 with AOTriton 0.8.1

--- cmake/External/aotriton.cmake
+++ cmake/External/aotriton.cmake
@@ -22,7 +22,7 @@ if(NOT __AOTRITON_INCLUDED)
   # Replaces .ci/docker/aotriton_version.txt
   # Note packages information may have versions skipped (due to no ABI breaks)
   # But they must be listed from lower version to higher version
-  set(__AOTRITON_VER "0.8b")
+  set(__AOTRITON_VER "0.8.1b")
   set(__AOTRITON_MANYLINUX_LIST
       "manylinux_2_28"  # rocm6.2
       "manylinux_2_28"  # rocm6.3
@@ -31,10 +31,10 @@ if(NOT __AOTRITON_INCLUDED)
       "rocm6.2"
       "rocm6.3"
       )
-  set(__AOTRITON_CI_COMMIT "6f8cbcac8a92775291bb1ba8f514d4beb350baf4")
+  set(__AOTRITON_CI_COMMIT "3a80554a88ae3b1bcf4b27bc74ad9d7b913b58f6")
   set(__AOTRITON_SHA256_LIST
-      "e938def5d32869fe2e00aec0300f354c9f157867bebdf2e104d732b94cb238d8"  # rocm6.2
-      "dc03d531ca399250b7d2fbdfa61929871788c6faeb7e462288e2a026e6b1e43d"  # rocm6.3
+      "8780410c95ce8f4057e470dea8fe98f977992ea9e21c0a5b14a3064b37a82033"  # rocm6.2
+      "02637956662f65a9f15670c0cc9d06c956710be1421892c84c240fbcd09edc51"  # rocm6.3
       )
   set(__AOTRITON_Z "gz")
 
