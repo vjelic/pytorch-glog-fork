8d1e5ebd7e0cfb7ce52cc37e6f86c57214fddd08
saienduri <saimanas.enduri@amd.com>
2025-07-10 01:22:12 -0700

    copy common utils as well in docker

--- .ci/docker/common/install_rocm.sh
+++ .ci/docker/common/install_rocm.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-source "$(dirname "${BASH_SOURCE[0]}")/common_utils.sh"
-
 ver() {
     printf "%3d%03d%03d%03d" $(echo "$1" | tr '.' ' ');
 }
@@ -208,10 +206,10 @@ EOF
     python3-dev \
     libegl1-mesa-dev
   
-  pip_install \
+  /opt/conda/bin/python -m pip install \
     --index-url https://d2awnip2yjpvqn.cloudfront.net/v2/gfx950-dcgpu/ \
     'rocm[libraries,devel]'
-  conda_run python -m rocm_sdk path --root
+  /opt/conda/bin/python -m rocm_sdk path --root
 }
 
 # Check for gfx950 architecture
