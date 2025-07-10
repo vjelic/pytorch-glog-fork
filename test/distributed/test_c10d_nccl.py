f18813e300ecb230b718e0e482c2e3cb2f871b34
Akash Verma <Akash.Verma3@amd.com>
2025-07-10 18:02:54 +0000

    Changed comment for the sleep statement added before collectives in test_extra_cuda_context test.

--- test/distributed/test_c10d_nccl.py
+++ test/distributed/test_c10d_nccl.py
@@ -592,10 +592,10 @@ class ProcessGroupNCCLGroupTest(MultiProcessTestCase):
         # Rank 0 takes a snapshot before collective -- this snapshot should have
         # included rank 0's own context.
         if self.rank == 0:
-            # Adding sleep statement (for Navi arch) before collectives as it helps 
-            # with memory allocation. Without sleep statement before collectives, the 
-            # torch.cuda.mem_get_info after collectives results in high memory usage
-            # resulting in test_extra_cuda_context test to fail. 
+            # Adding sleep statement (for Navi arch) before collectives as init_process_group 
+            # has an extra process which is taking time to finish on navi Without sleep statement 
+            # before collectives, the torch.cuda.mem_get_info after collectives results in high 
+            # memory usage resulting in test_extra_cuda_context test to fail. 
             if is_arch(NAVI_ARCH):
                 time.sleep(5)
             free, total = torch.cuda.mem_get_info(device)
