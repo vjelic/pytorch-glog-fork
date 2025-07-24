6ad2a5d97b47b028a13af5d439c538afb3936a1c
Ethan Wee <Ethan.Wee@amd.com>
2025-07-24 13:44:10 -0700

    check cmake

--- .ci/pytorch/build.sh
+++ .ci/pytorch/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 set -ex
-
+CMAKE=/usr/local/bin/cmake
+[ -x "$CMAKE" ] || CMAKE=/usr/bin/cmake
+cmake() { "$CMAKE" "$@"; }
 # Required environment variable: $BUILD_ENVIRONMENT
 # (This is set by default in the Docker images we build, so you don't
 # need to set it yourself.
