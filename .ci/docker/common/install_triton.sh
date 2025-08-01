9fe93933b6ed70cb19562337148948418658f55b
Ethan Wee <Ethan.Wee@amd.com>
2025-08-01 07:42:27 -0700

    Update install_triton.sh

--- .ci/docker/common/install_triton.sh
+++ .ci/docker/common/install_triton.sh
@@ -22,7 +22,7 @@ elif [ -n "${TRITON_CPU}" ]; then
   TRITON_REPO="https://github.com/triton-lang/triton-cpu"
   TRITON_TEXT_FILE="triton-cpu"
 else
-  TRITON_REPO="https://github.com/ROCm/triton"
+  TRITON_REPO="https://github.com/triton-lang/triton"
   TRITON_TEXT_FILE="triton"
 fi
 
@@ -78,7 +78,6 @@ cp dist/*.whl /opt/triton
 # Install the wheel for docker builds that don't use multi stage
 pip_install dist/*.whl
 
-<<<<<<< HEAD
 # TODO: This is to make sure that the same cmake and numpy version from install conda
 # script is used. Without this step, the newer cmake version (3.25.2) downloaded by
 # triton build step via pip will fail to detect conda MKL. Once that issue is fixed,
